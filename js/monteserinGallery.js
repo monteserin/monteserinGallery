23ab2e2a09403ca240f21f22b32a810855aaf7ce
gitpablo <monty@monty-ThinkPad-Edge.(none)>
2013-04-03 22:28:05 +0200

    modified:   css/monteserinGallery.css 	modified:   index.html 	modified:   js/monteserinGallery.js 	modified:   monteserinGallery.jquery.json

--- js/monteserinGallery.js
+++ js/monteserinGallery.js
@@ -1,12 +1,13 @@
 jQuery.fn.monteserinGallery = function(event){
 	var counter = -1;
+	var counterNextImg = -1;
+	var imagesArray;
 	var initPosX = 0
 	var initPosY = 0;
 	var displayPosX = 0;
 	var displayPosY = 0;
 	var endPosX = 0;
 	var endPosY = 0;
-	var initWidth = 100;
 	var inTime = 500;
 	var outTime = 500;
 	var displayTime = 1000;
@@ -15,24 +16,21 @@ jQuery.fn.monteserinGallery = function(event){
 	if(event.initPosY)initPosY = event.initPosY;
 	if(event.endPosX)endPosX = event.endPosX;
 	if(event.endPosY)endPosY = event.endPosY;
-	if(event.initWidth)initWidth = event.initWidth;
 	if(event.inTime)inTime = event.inTime;
 	if(event.outTime)outTime = event.outTime;
 	if(event.displayTime)displayTime = event.displayTime;
 	size = $("li", this).length;
-	$("img", this).css({"margin-left" : initPosX+"px", "position" : "absolute", "width" : initWidth+"px", "opacity" : "0"});
-	loop(this);
+		imagesArray = $("img", this),
 
-	function loop(me){
-		if(counter>=size-1){counter=0;}
-		else{ counter++;}
+	imagesArray.css({"opacity" : "0"});
+	loop( counter);
 
-		$("img", me).eq(counter)
-	//top, right, bottom, left
-	.animate({margin: displayPosY+"px 0px 0px "+displayPosX+"px", opacity:"1"},inTime).delay(displayTime)
-	.animate({margin: endPosY+"px 0px 0px "+endPosX+"px", opacity:"0"},outTime)
-	//reset image
-	.animate({margin: initPosY+"px 0px 0px "+initPosX+"px"},0,function(){loop(me)});
+	function loop(counter){
+		counter++;
+		if(counter>=size){counter=0;}
+imagesArray.eq(counter).css({"margin-left" : initPosX});
+	imagesArray.eq(counter).animate({margin: displayPosY+"px 0px 0px "+displayPosX+"px", opacity:"1"},inTime).delay(displayTime).queue(function(nxt){loop(counter); imagesArray.eq(counter).animate({margin: endPosY+" 0px 0px "+endPosX, opacity:"0"},outTime); nxt() })
+
 }
 }
 
