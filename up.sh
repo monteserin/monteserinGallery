e855948b95a2e1e564d017dcd26f41485e594cdb
monty <monty@monty-ThinkPad-Edge.(none)>
2013-06-10 09:00:19 +0200

    modified:   index.html

--- up.sh
+++ up.sh
@@ -6,3 +6,11 @@ git remote add origin https://github.com/monteserin/monteserinGallery.git
 
 git tag 1.1.4 #esta versión debe coincidir con la del manifest
 git push origin --tags
+
+
+
+ ! [rejected]        master -> master (non-fast-forward)
+error: failed to push some refs to ' '
+To prevent you from losing history, non-fast-forward updates were rejected
+Merge the remote changes (e.g. 'git pull') before pushing again.  See the
+'Note about fast-forwards' section of 'git push --help' for details.
