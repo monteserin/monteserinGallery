55d98033f10254e7250b6604635185b2309d26df
pablomonteserin@pablomonteserin.com <pp@pp.com>
2014-07-30 10:53:20 +0200

    plugin

--- js/monteserinGallery.js
+++ js/monteserinGallery.js
@@ -2,25 +2,32 @@ jQuery.fn.monteserinGallery = function(event){
 	var counter = -1;
 	var counterNextImg = -1;
 	var imagesArray;
-	var initPosX = 0
+	var initPosX = -300
 	var initPosY = 0;
 	var displayPosX = 0;
 	var displayPosY = 0;
-	var endPosX = 0;
+	var endPosX = 300;
 	var endPosY = 0;
-	var inTime = 500;
-	var outTime = 500;
-	var displayTime = 1000;
-
-	if(event.initPosX)initPosX = event.initPosX;
-	if(event.initPosY)initPosY = event.initPosY;
-	if(event.endPosX)endPosX = event.endPosX;
-	if(event.endPosY)endPosY = event.endPosY;
-	if(event.inTime)inTime = event.inTime;
-	if(event.outTime)outTime = event.outTime;
-	if(event.displayTime)displayTime = event.displayTime;
+	var inTime = 1000;
+	var outTime = 1000;
+	var displayTime = 2500;
+	$this = $(this);
+	$thisFirstChild = $this.find("img:first-child");
+	$this.css({"width": $thisFirstChild.width()+"px","height": $thisFirstChild.height()+"px", "position":"relative"});
+	$this.find("li").css({"margin": "0", "padding": "0"});
+	$this.find("img").css({"position":"absolute","box-shadow": "10px 50px 100px black", });
+	if(event !== undefined){
+		if(event.initPosX !== undefined)initPosX = event.initPosX;
+		if(event.initPosY !== undefined)initPosY = event.initPosY;
+		if(event.endPosX !== undefined)endPosX = event.endPosX;
+		if(event.endPosY !== undefined)endPosY = event.endPosY;
+		if(event.inTime !== undefined)inTime = event.inTime;
+		if(event.outTime !== undefined)outTime = event.outTime;
+		if(event.displayTime !== undefined)displayTime = event.displayTime;
+	}
+	
 	size = $("li", this).length;
-		imagesArray = $("img", this),
+	imagesArray = $("img", this),
 
 	imagesArray.css({"opacity" : "0"});
 	loop( counter);
@@ -28,9 +35,9 @@ jQuery.fn.monteserinGallery = function(event){
 	function loop(counter){
 		counter++;
 		if(counter>=size){counter=0;}
-imagesArray.eq(counter).css({"margin-left" : initPosX,"margin-top":initPosY});
-	imagesArray.eq(counter).animate({margin: displayPosY+"px 0px 0px "+displayPosX+"px", opacity:"1"},inTime).delay(displayTime).queue(function(nxt){loop(counter); imagesArray.eq(counter).animate({margin: endPosY+" 0px 0px "+endPosX, opacity:"0"},outTime); nxt() })
+		imagesArray.eq(counter).css({"margin-left" : initPosX,"margin-top":initPosY});
+		imagesArray.eq(counter).animate({margin: displayPosY+"px 0px 0px "+displayPosX+"px", opacity:"1"},inTime).delay(displayTime).queue(function(nxt){loop(counter); imagesArray.eq(counter).animate({margin: endPosY+" 0px 0px "+endPosX, opacity:"0"},outTime); nxt() })
 
-}
+	}
 }
 
